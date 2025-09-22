f3cf24e33b87804ec547acb9231608e68bceae66
user17 <sayanray2003@gmail.com>
2025-09-22 11:33:34 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,14 @@
+
+
+fruits = ['apple', 'banana', 'cherry']
+
+#add
+fruits.append('orange')
+fruits.insert(1,'mango')
+
+print(fruits)
+
+#remove
+fruits.remove('banana')
+fruits.pop()
+print(fruits)
